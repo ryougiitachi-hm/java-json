f618d94cdc953a5e21848542dfc3dfedaf7ef295
ryougiitachi <ryougiitachi@hotmail.com>
2022-01-28 23:02:09 +0800

    json-gson

---
+++ json-gson/src/main/java/per/itachi/java/json/gson/dto/UserDto.java
@@ -0,0 +1,15 @@
+package per.itachi.java.json.gson.dto;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    @SerializedName("first_name")
+    private String firstName;
+
+    private int height;
+}
