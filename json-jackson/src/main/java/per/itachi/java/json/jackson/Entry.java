3231acff11c15fe4aa6e394fdb165f687c2c3279
ryougiitachi-hm <ryougiitachi@hotmail.com>
2022-08-13 21:39:24 +0800

    [json-jackson] Added test cases to json-jackson.

--- json-jackson/src/main/java/per/itachi/java/json/jackson/Entry.java
+++ json-jackson/src/main/java/per/itachi/java/json/jackson/Entry.java
@@ -5,12 +5,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class Entry {
 
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
+//        testBase64(objectMapper);
+        testBytes(objectMapper);
+
+    }
+
+    private static void testBase64(ObjectMapper objectMapper) {
         byte[] bytesDocumentData = new byte[10];
         try {
             byte[] bytesResult = objectMapper.writeValueAsBytes(bytesDocumentData);
@@ -18,6 +26,20 @@ public class Entry {
             log.info("strResult is {}", strResult);
             strResult = Base64.getEncoder().encodeToString(bytesDocumentData);
             log.info("strResult is {}", strResult);
+        }
+        catch (JsonProcessingException e) {
+            log.error("", e);
+        }
+    }
+
+    private static void testBytes(ObjectMapper objectMapper) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", new byte[10]);
+        map.put("code", 200);
+        try {
+            // byte[] will be converted as base64 string automatically.
+            String strJson = objectMapper.writeValueAsString(map);
+            log.info("strJson is {}", strJson);
         }
         catch (JsonProcessingException e) {
             log.error("", e);
